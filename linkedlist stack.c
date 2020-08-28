0ad4097f4197971f968f4a73a5926b8fd1a52199
yurajcs18 <54244837+yurajcs18@users.noreply.github.com>
2020-08-28 15:44:45 +0530

    Add files via upload

---
+++ linkedlist stack.c
@@ -0,0 +1,79 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node *next;
+}*NODE;
+
+
+NODE getnode(){
+	return (NODE)malloc(sizeof(struct node));
+}
+
+
+NODE insertFront(NODE head, int item){
+    NODE p;
+    p = getnode();
+    p->data = item;
+    p->next = head;
+    head = p;
+    return head;
+}
+
+
+NODE deleteFront(NODE head){
+	NODE p;
+	if(head == NULL)
+		printf("Stack is empty");
+	else{
+		printf("Deleted from stack %d\n", head->data);
+		p = head->next;
+		free(head);
+		return p;
+	}
+}
+
+
+void display(NODE head){
+    NODE p;
+    if(head == NULL){
+        printf("Stack empty\n");
+        return;
+    }
+    p = head;
+    printf("STACK  ");
+    while(p != NULL){
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+
+void main()
+{
+    NODE head = NULL;
+    int c, ele;
+
+    while(1){
+        printf("\n\n1.Push\n2.Pop\n5.Exit\n");
+        scanf("%d", &c);
+        switch(c){
+            case 1:
+                printf("Element is:");
+                scanf("%d", &ele);
+                head = insertFront(head, ele);
+                display(head);
+                break;
+            
+            case 2:
+                head = deleteFront(head);
+                display(head);
+                break;
+
+            case 5:
+                exit(1);		
+        }
+    }
+}
